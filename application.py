0c03aaf98409f773e171b4f530f88710b604ffe8
juanzapa <juanzapa@bancolombia.com.co>
2021-01-12 15:08:30 -0500

    Cambio para activar pipeline

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def index():
-    return 'Hello World!'
+    return 'Hello World! con cambio desde mi PC'
 
 if __name__ == '__main__':
     application.run(host="0.0.0.0", port=80, debug = True)
