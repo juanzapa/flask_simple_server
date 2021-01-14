6d8c8c2ef3bbd0513a4f66c1fe663c562fcce108
juanzapa <juanzapa@bancolombia.com.co>
2021-01-14 15:07:57 -0500

    add: new message 6.0

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def index():
-    return 'Hello World! con el pipeline del 2021 con mucha alegria y probando notificación 4.0'
+    return 'Hello World! con el pipeline del 2021 con mucha alegria y probando notificación 5.0'
 
 if __name__ == '__main__':
     application.run(host="0.0.0.0", port=80, debug = True)
